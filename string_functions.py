f8dccf9ca3df26c2c727904f0ec4872b56e46a14
Biswas Poudel <biswas.poudel13@gmail.com>
2024-02-12 09:11:19 -0600

    Initial commit, added two files

---
+++ string_functions.py
@@ -0,0 +1,8 @@
+quote = "to be or not to be"
+
+# strings are immutable in python -- meaning they can't be changed 
+# But you can reassign them to a new value
+quote2 = quote.replace('be', 'me')
+print(quote.upper())
+print(quote.capitalize())
+print(quote2)
---
+++ type_conversion_exercise.py
@@ -0,0 +1,4 @@
+birth_year = input("What year were you born? ")
+age = 2024 - int(birth_year)
+
+print(f"You are {age} years old.")
